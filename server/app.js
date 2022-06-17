bc8cfe4fdb74bf1c6f46e4d34f528a9c168f9e51
Павел <pkazhgin@gmail.com>
2022-06-17 15:26:52 +0300

    ws

--- server/app.js
+++ server/app.js
@@ -3,6 +3,13 @@ const path = require('path');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 
+const http = require('http');
+const { v4: uuidv4 } = require('uuid');
+
+const { WebSocketServer } = require('ws');
+
+const map = new Map(); //(Хранение данных. Возвращает ключ(id) => значение(браузерное соединение пользователя))
+
 const app = express();
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
@@ -15,6 +22,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(process.env.PWD, 'public')));
 app.use(cookieParser());
 
-app.listen(PORT, () => {
-  console.log(`Server is started on port: ${PORT}`);
+const server = http.createServer(app);
+
+
+server.listen(PORT, () => {
+  console.log('server start on', PORT);
 });
