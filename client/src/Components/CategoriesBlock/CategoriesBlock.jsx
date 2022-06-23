064d918b516adb8aa454c621d8c913927fcc75a7
Elizaveta Zimonina <lisaliskin@gmail.com>
2022-06-23 13:12:09 +0300

    правки по стилю и гл стр

--- client/src/Components/CategoriesBlock/CategoriesBlock.jsx
+++ client/src/Components/CategoriesBlock/CategoriesBlock.jsx
@@ -4,53 +4,130 @@ import { Col } from 'reactstrap';
 export default function CategoriesBlock() {
   return (
     <Col className="col-3">
+      <h5>Категории добрых дел</h5>
+      <hr />
       <div className="accordion accordion-flush" id="accordionFlushExample">
         <div className="accordion-item">
           <h2 className="accordion-header" id="flush-headingOne">
             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-              Accordion Item #1
+              помощь по дому
             </button>
           </h2>
           <div id="flush-collapseOne" className="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to demonstrate the
-              {' '}
-              <code>.accordion-flush</code>
-              {' '}
-              class. This is the first accordion body.
-            </div>
+            <ul className="accordion-body list-unstyled">
+              <li className="mb-1">уборка</li>
+              <li className="mb-1">готовка</li>
+              <li className="mb-1">ремонт</li>
+            </ul>
           </div>
         </div>
         <div className="accordion-item">
           <h2 className="accordion-header" id="flush-headingTwo">
             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-              Accordion Item #2
+              няньки/сиделки
             </button>
           </h2>
           <div id="flush-collapseTwo" className="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to demonstrate the
-              {' '}
-              <code>.accordion-flush</code>
-              {' '}
-              class. This is the second accordion body. Let's imagine this being filled with some actual content.
-            </div>
+            <ul className="accordion-body list-unstyled">
+              <li className="mb-1">посидеть с ребенком</li>
+              <li className="mb-1">позаботиться о пожилом человеке</li>
+            </ul>
           </div>
         </div>
         <div className="accordion-item">
           <h2 className="accordion-header" id="flush-headingThree">
             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
-              Accordion Item #3
+              животные
             </button>
           </h2>
           <div id="flush-collapseThree" className="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
-            <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to demonstrate the
-              {' '}
-              <code>.accordion-flush</code>
-              {' '}
-              class. This is the third accordion body. Nothing more exciting happening here in terms of content, but just filling up the space to make it look, at least at first glance, a bit more representative of how this would look in a real-world application.
-            </div>
+            <ul className="accordion-body list-unstyled">
+              <li className="mb-1">выгул собаки</li>
+              <li className="mb-1">передержка котика</li>
+              <li className="mb-1">передержка пёсика</li>
+            </ul>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="flush-headingFour">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+              одолжить
+            </button>
+          </h2>
+          <div id="flush-collapseFour" className="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+            <ul className="accordion-body list-unstyled">
+              <li className="mb-1">отдать даром</li>
+              <li className="mb-1">дать погонять</li>
+            </ul>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="flush-headingFive">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+              репетиторство
+            </button>
+          </h2>
+          <div id="flush-collapseFive" className="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+            <ul className="accordion-body list-unstyled">
+              <li className="mb-1">помощь с д/з</li>
+              <li className="mb-1">профессиональные задачи</li>
+              <li className="mb-1">иностранные языки</li>
+            </ul>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="flush-headingSix">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
+              транспорт
+            </button>
+          </h2>
+          <div id="flush-collapseSix" className="accordion-collapse collapse" aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
+            <ul className="accordion-body list-unstyled">
+              <li className="mb-1">переезд</li>
+              <li className="mb-1">ремонт(велик, скейт и тд)</li>
+              <li className="mb-1">подвезти</li>
+            </ul>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="flush-headingSeven">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeven" aria-expanded="false" aria-controls="flush-collapseSeven">
+              общение
+            </button>
+          </h2>
+          <div id="flush-collapseSeven" className="accordion-collapse collapse" aria-labelledby="flush-headingSeven" data-bs-parent="#accordionFlushExample">
+            <ul className="accordion-body list-unstyled">
+              <li className="mb-1">психологическая помощь</li>
+              <li className="mb-1">просто поговорить</li>
+            </ul>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="flush-headingEight">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseEight" aria-expanded="false" aria-controls="flush-collapseEight">
+              красота и здоровье
+            </button>
+          </h2>
+          <div id="flush-collapseEight" className="accordion-collapse collapse" aria-labelledby="flush-headingEight" data-bs-parent="#accordionFlushExample">
+            <ul className="accordion-body list-unstyled">
+              <li className="mb-1">прически/стрижка</li>
+              <li className="mb-1">массаж</li>
+              <li className="mb-1">макияж</li>
+              <li className="mb-1">спорт</li>
+            </ul>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header" id="flush-headingNine">
+            <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseNine" aria-expanded="false" aria-controls="flush-collapseNine">
+              фотосессии/видеосъемка
+            </button>
+          </h2>
+          <div id="flush-collapseEight" className="accordion-collapse collapse" aria-labelledby="flush-headingEight" data-bs-parent="#accordionFlushExample">
+            <ul className="accordion-body list-unstyled">
+              <li className="mb-1">фотосессии</li>
+              <li className="mb-1">видеосъемка</li>
+            </ul>
           </div>
         </div>
       </div>
